6a0fa1490ed6690318b2e7a5cd178dea3c0eac10
shaima <chaimabenboubaker1@gmail.com>
2022-07-06 22:34:51 -1200

    TD-1 Create and run express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+require('dotenv/config');
+const express = require('express');
+
+const config = require('./config');
+
+const app = express();
+
+app.listen(config.port, () =>
+  console.log('Server is running at port %d', config.port)
+);
