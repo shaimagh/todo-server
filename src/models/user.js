6cebd3ab06e1957388a27fb3071c142f412d1815
shaima <chaimabenboubaker1@gmail.com>
2022-07-06 23:00:28 -1200

    TD-2 Create user and todo models

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+const { model, Schema } = require('mongoose');
+const { genSaltSync, hashSync, compareSync } = require('bcryptjs');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  }
+});
+
+userSchema.pre('save', function (next) {
+  if (this.isModified('password') && this.isNew) {
+    const salt = genSaltSync(10);
+    this.password = hashSync(this.password, salt);
+  }
+
+  next();
+});
+
+userSchema.methods.isValidPassword = function (password) {
+  return compareSync(password, this.password);
+};
+
+module.exports = model('users', userSchema);
