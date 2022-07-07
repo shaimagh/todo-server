51fb0a49884307d768ba6910609dd278546b768a
shaima <chaimabenboubaker1@gmail.com>
2022-07-07 00:14:15 -1200

    TD-3 Connect to db and integrate user router with express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 require('dotenv/config');
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const config = require('./config');
+const { UserRouter } = require('./routers');
 
-const app = express();
+async function startServer() {
+  await mongoose.connect(config.mongodbUri);
+  console.log('Connected to database');
 
-app.listen(config.port, () =>
-  console.log('Server is running at port %d', config.port)
-);
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use('/auth', UserRouter);
+
+  app.listen(config.port, () =>
+    console.log('Server is running at port %d', config.port)
+  );
+}
+
+startServer();
