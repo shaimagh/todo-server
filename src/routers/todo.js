b42ab868cf04ef4fc5a330e4528258808f72e258
shaima <chaimabenboubaker1@gmail.com>
2022-07-08 14:20:33 -1200

    TD-4 Update user routes

--- src/routers/todo.js
+++ src/routers/todo.js
@@ -1,11 +1,34 @@
 const { Router } = require('express');
+const { body, param } = require('express-validator');
+
 const { TodoController } = require('../controllers');
+const { isObjectId, validateRequestInput } = require('../utils/validator');
 
 const TodoRouter = Router();
 
 TodoRouter.get('/', TodoController.getTodos);
-TodoRouter.post('/', TodoController.createTodo);
-TodoRouter.put('/:id', TodoController.updateTodo);
-TodoRouter.delete('/:id', TodoController.deleteTodo);
+TodoRouter.post(
+  '/',
+  body('title').exists().isString(),
+  body('description').isString(),
+  body('date').isDate({ format: 'dd-mm-yyyy' }),
+  validateRequestInput,
+  TodoController.createTodo
+);
+TodoRouter.put(
+  '/:id',
+  param('id').custom(isObjectId),
+  body('title').exists().isString(),
+  body('description').isString(),
+  body('date').isDate({ format: 'dd-mm-yyyy' }),
+  validateRequestInput,
+  TodoController.updateTodo
+);
+TodoRouter.delete(
+  '/:id',
+  param('id').custom(isObjectId),
+  validateRequestInput,
+  TodoController.deleteTodo
+);
 
 module.exports = TodoRouter;
